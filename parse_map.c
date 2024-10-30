cbef8c040f1f093bac0e4d1e9663ce6eefc5675c
Oleh Ruban <oruban@c2r9s2.42wolfsburg.de>
2024-10-30 21:04:58 +0100

    parse_map.c partly refactured

--- parse_map.c
+++ parse_map.c
@@ -6,7 +6,7 @@
 /*   By: oruban <oruban@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 14:18:11 by rkrechun          #+#    #+#             */
-/*   Updated: 2024/10/28 21:44:48 by oruban           ###   ########.fr       */
+/*   Updated: 2024/10/30 21:03:20 by oruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	return (b);
 }
 
-void	open_file(char *map_path, t_data *dat)
+/* void	open_file(char *map_path, t_data *dat)
 {
 	int		fd;
 	char	*line;
@@ -98,6 +98,38 @@ void	open_file(char *map_path, t_data *dat)
 	}
 	dat->map[i] = NULL;
 	close(fd);
+} */
+
+/* 
+	The open_file function in your cub3D project is responsible for opening a 
+	map file, reading its contents, and storing the relevant lines in the 
+	dat->map array. - refacktored by 3010 roi
+ */
+
+void	open_file(char *map_path, t_data *dat)
+{
+	int		fd;
+	char	*line;
+	int		i;
+
+	count_line(map_path, dat);
+	fd = open(map_path, O_RDONLY);
+	printf("%i\n", dat->map_height);
+	dat->map = malloc(sizeof(char *) * (dat->map_height + 1));
+	if (!dat->map)
+		return ;
+	i = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (line[0] == '1' || line[0] == '0' || line[0] == ' '
+			|| line[0] == '\t')
+			dat->map[i++] = ft_strdup(line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	dat->map[i] = NULL;
+	close(fd);
 }
 
 void	check_pos(t_data *data, int i, int j)
@@ -124,7 +156,7 @@ void	change_pos(t_data *data, int i, int j)
 		data->player_y = (float)i;
 }
 
-void	player_position(t_data *dat)
+/* void	player_position(t_data *dat)
 {
 	int	i;
 	int	j;
@@ -154,4 +186,36 @@ void	player_position(t_data *dat)
 		printf("Error\nNo player position\n");
 		return ;
 	}
+} */
+
+/* 
+	The  function  is responsible for finding the initial position of the player
+	on the map, setting the player's coordinates, and updating the map to
+	reflect the player's starting position.  
+		- refacktored by 3010 roi
+*/
+void	player_position(t_data *dat)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (++i < dat->map_height)
+	{
+		j = -1;
+		while (dat->map[i][++j])
+		{
+			if (dat->map[i][j] == 'N' || dat->map[i][j] == 'S' ||
+				dat->map[i][j] == 'W' || dat->map[i][j] == 'E')
+			{
+				change_pos(dat, i, j);
+				dat->player_x = (float)j + 0.5;
+				dat->player_y = (float)i + 0.5;
+				dat->map[i][j] = '0';
+				return ;
+			}
+		}
+	}
+	if (i == dat->map_height)
+		printf("Error\nNo player position\n");
 }
